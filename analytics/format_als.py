9aa78049c0d1861262eb1f618f1fae8893316147
Peng <pengwu100@gmail.com>
2017-04-21 11:42:30 -0400

    string id to int id

---
+++ analytics/format_als.py
@@ -0,0 +1,52 @@
+
+import sys
+import csv
+
+
+print("sys.maxint = {}".format(sys.maxint))
+print("The program use 2^32 as the MAXINT.")
+MAXINT = 2**32
+
+
+def strID_to_intID(strID):
+    return hash(strID) % MAXINT
+
+
+inputFileFolder = "../../csv-2013-10-25/"
+
+######
+
+output = open(inputFileFolder + "addrs_intid.csv", 'w')
+writer = csv.writer(output)
+with open(inputFileFolder + "addrs.csv") as f:
+    old = []
+    new = []
+    reader = csv.reader(f)
+    for row in reader:
+        from_addr = strID_to_intID(row[0])
+        to_addr = strID_to_intID(row[1])
+        value = float(row[2])
+        tx_hash = strID_to_intID(row[3])
+
+        old.append(row[0])
+        new.append(from_addr)
+
+        #print [from_addr, to_addr, value, tx_hash]
+        writer.writerow([from_addr, to_addr, value, tx_hash])
+output.close()
+print(len(set(old)), len(set(new)))
+print("# addrs.csv Done")
+
+######
+
+output = open(inputFileFolder + "transactions_intid.csv", 'w')
+writer = csv.writer(output)
+with open(inputFileFolder + "transactions.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        tx_hash = strID_to_intID(row[0])
+        row[0] = tx_hash
+        writer.writerow(row)
+output.close()
+print("# transactions.csv Done")
+
